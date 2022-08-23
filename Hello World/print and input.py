7f790ecf0674f9291b18a78c12bf32b9abe9336a
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-23 13:57:18 -0300

    Update print and input.py

--- Hello World/print and input.py
+++ Hello World/print and input.py
@@ -1,3 +1,10 @@
 // toda variável é um objeto
 // usar "vírgula(,)" para printar variáveis de tipos diferentes
 
+nome = input('Qual é o seu nome? ')
+print(f'Olá, {nome}! Seja bem-vindo!')
+
+dia = input('\nQue dia é seu aniversário? ')
+mês = input('De qual mês? ')
+ano = input('E em que ano você nasceu? ')
+print(f'Você nasceu em {dia} de {mês} de {ano}.')
