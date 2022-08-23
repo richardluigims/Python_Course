0eb20746c15102cf9daddfaad45765f5ce56e069
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-23 14:26:37 -0300

    Update Is.py

--- Hello World/Is.py
+++ Hello World/Is.py
@@ -1,5 +1,10 @@
 n = input('Digite algo: ')
 
-print(n.isnumeric()) # números
-print(n.isalpha()) # letras
-print(n.isalnum()) # letras e números (juntos = 7a), letras ou números
+print('O tipo primitivo desse valor é',type(n))
+print('É um número?', n.isnumeric())
+print('É alfabético?', n.isalpha())
+print('É alfanumérico?', n.isalnum()) # letras e números (juntos = 7a), letras ou números
+print('Tem apenas espaços?', n.isspace())
+print('Está em maiúsculas?', n.isupper())
+print('Está em minúsculas?', n.islower())
+print('Está capitalizada?', n.istitle()) # maiúsculas e minúsculas
