f5d36a0458d422c0374fba137c99a3b01a5e0fc3
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-23 13:57:35 -0300

    Create soma.py

---
+++ Hello World/soma.py
@@ -0,0 +1,6 @@
+print('\nVamos somar dois números!')
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+soma = num1 + num2
+
+print(f'A sua soma é igual a {soma}')
