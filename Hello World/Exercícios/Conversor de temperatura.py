125ac0a4afb682663e631c4fb7f33e0d1ac0b144
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-24 16:09:09 -0300

    Update Conversor de temperatura.py

--- Hello World/Exercícios/Conversor de temperatura.py
+++ Hello World/Exercícios/Conversor de temperatura.py
@@ -1,4 +1,4 @@
 C = float(input('Informe a temperatura em °C: '))
 F = ((9*C)/5)+32
 
-print(f'A temperatura de {C}°C corresponde a {F}°F.')
+print(f'A temperatura de {C:.2f}°C corresponde a {F:.2f}°F.')
