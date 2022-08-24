86427aa478081c18c12b0b73cc25cfba18478421
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-24 14:46:05 -0300

    Update soma.py

--- Hello World/soma.py
+++ Hello World/soma.py
@@ -4,3 +4,9 @@ num2 = int(input('Digite o segundo número: '))
 soma = num1 + num2
 
 print(f'A sua soma é igual a {soma}')
+
+#sem variável soma:
+n1 = int(input('valor 1: '))
+n2 = int(input('valor 2: '))
+
+print(f'A soma vale {n1+n2}')
