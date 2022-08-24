221cdfddb97fc60e9cbc55e0ecb190b272e594c8
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-24 15:58:59 -0300

    Update Descontos e aumentos.py

--- Hello World/Exercícios/Descontos e aumentos.py
+++ Hello World/Exercícios/Descontos e aumentos.py
@@ -4,6 +4,6 @@ desconto = n1 - ((n1 * 5)/100)
 print(f'Na promoção seu produto custa R${desconto:.2f}.')
 
 n2 = float(input('Digite seu salário: '))
-Aumento = n2 - ((n2 * 15)/100)
+Aumento = n2 + ((n2 * 15)/100)
 
-print(f'Seu novo salário é R${n2+Aumento:.2f}.')
+print(f'Seu novo salário é R${Aumento:.2f}.')
