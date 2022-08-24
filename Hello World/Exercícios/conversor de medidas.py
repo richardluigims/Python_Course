674a51360149216f727d0ba47e37d9da931c8b89
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-24 15:46:02 -0300

    Update conversor de medidas.py

--- Hello World/Exercícios/conversor de medidas.py
+++ Hello World/Exercícios/conversor de medidas.py
@@ -2,4 +2,4 @@ M = float(input('Digite um valor em metros: '))
 cm = M*100
 mm = M*1000
 
-print(f'{M:.2f} metro é igual a {cm:.2f} centímetros e {mm:.2f} milímetros.')
+print(f'{M:.2f} metro é igual a {cm:.0f} centímetros e {mm:.0f} milímetros.')
