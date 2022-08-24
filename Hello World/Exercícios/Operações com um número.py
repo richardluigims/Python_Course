92bda75aae0e79dececee889a97b5c437c114ded
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-24 15:41:04 -0300

    Create Operações com um número.py

---
+++ Hello World/Exercícios/Operações com um número.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um valor: '))
+
+print(f'\nO sucessor de {n1} é {n1+1} e o antecessor é {n1-1}.')
+print(f'O dobro de {n1} é {n1*2}, e o triplo é {n1*3}.\nA raíz quadrada é {n1**(1/2):.1f}')
