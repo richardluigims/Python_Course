189d441e3e123b5a6469127a47b365dd4c7f6004
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-25 04:05:03 -0300

    Create Fatiamento.py

---
+++ Manipulando Texto/Fatiamento.py
@@ -0,0 +1,8 @@
+n1 = 'Curso em Video Python'
+
+print(n1[9]) #printa a 10° letra
+print(n1[9:13]) #printa a posição 9 até 12
+print(n1[9:21:2]) #printa a posição 9 até 20, pulando de 2 em 2
+print(n1[:5]) #printa posição 0 até 4
+print(n1[15:]) #printa posição 15 até o final
+print(n1[9::3]) #printa posição 9 até o final, pulando de 3 em 3
