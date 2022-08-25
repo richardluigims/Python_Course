97284fe642f567fee1da2cab5f8791ae2aeb139c
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-25 04:05:51 -0300

    Create Análise.py

---
+++ Manipulando Texto/Análise.py
@@ -0,0 +1,7 @@
+n1 = 'Curso em Video Python'
+
+print(len(n1)) #tamanho da frase
+print(n1.count('o')) #contar letra. SE ('o', 0, 13) ===> contar só até 12
+print(n1.find('deo')) #mostra onde começa o trecho nos parênteses
+print(n1.find('Android')) #retorna -1 quando a string dentro de ' ' não existe na variável
+print('Curso' in n1) #procura pelo termo dentro das ' '
