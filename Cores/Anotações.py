3a61a28973ae90ea2a9bf9918912628628fa04df
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-10-25 15:47:28 -0300

    Create Anotações.py

---
+++ Cores/Anotações.py
@@ -0,0 +1,12 @@
+\033[0;33;44m
+#primeiro valor: estilo (negrito, sublinhado, etc)
+#cor do texto
+#cor do fundo
+
+#estilos: 0(nenhum - também pode não colocar nada), 1(negrito), 4(sublinhado), 7(inverte as configurações - cor do texto se torna do fundo e vice-versa)
+
+#cor do texto: branco(30), vermelho(31), verde(32), amarelo(33), azul(34), roxo(35), ciano(36), cinza(37)
+
+#cor do fundo: igual a texto, porém 40 --- 47]
+#\033[m ---> reinicia o terminal para as cores padrão
+#\033[7;30m ---> letra preta e fundo branco
